1aea46f8c58c15fcac6a5c4528205dc179728bbf
sibx2307 <masimbagain2@gmail.com>
2026-06-14 21:09:59 +0530

    first commit

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+    DATABASE_URL = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
